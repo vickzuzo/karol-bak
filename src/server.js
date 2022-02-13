24aa2288fb5522c00b026e0fc20e485ff0e4a957
vickzuzo <vickzuzo@gmail.com>
2022-02-13 22:19:48 +0100

    update

--- src/server.js
+++ src/server.js
@@ -28,9 +28,17 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const server = app.listen(PORT, () =>
   console.log(`Server running on PORT: ${PORT}`)
 );
+
 
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Logged Error: ${err}`);
