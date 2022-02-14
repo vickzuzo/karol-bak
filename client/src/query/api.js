c960010588cd07851e72af4d999abaa79ac9894f
vickzuzo <vickzuzo@gmail.com>
2022-02-14 09:27:52 +0100

    fixed heroku error

--- client/src/query/api.js
+++ client/src/query/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "",
   headers: {
     "Content-type": "application/json",
     accept: "*/*",
